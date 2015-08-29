7a982201757d74615ad151a3b8a37505c3aca521
toro23 <23@coidex.com>
2015-08-29 16:25:06 +0000

    テストデータはFabricateを使って生成するように変更

--- app/Test/Case/Controller/PostsControllerTest.php
+++ app/Test/Case/Controller/PostsControllerTest.php
@@ -1,5 +1,6 @@
 <?php
 App::uses('PostsController', 'Controller');
+App::uses('Fabricate', 'Fabricate.Lib');
 
 /**
  * PostsController Test Case
@@ -26,12 +27,10 @@ class PostsControllerTest extends ControllerTestCase {
 		$this->controller->autoRender = false;
 	}
 	public function testIndexアクションではページングの結果がpostsにセットされること(){
-		$data = [
-			['Posts'=>['id'=>1,'title'=>'Title1','body'=>'Body1']],
-		];
-		$this->controller->Paginator->expects($this->once())->method('paginate')->will($this->returnValue($data));
+		$post = Fabricate::build('Post');
+		$this->controller->Paginator->expects($this->once())->method('paginate')->will($this->returnValue($post->data));
 		$vars = $this->testAction('/user/blog', ['method'=>'get', 'return'=>'vars']);
-		$this->assertEquals($data, $vars['posts']);
+		$this->assertEquals($post->data, $vars['posts']);
 	}
 	public function testAddアクションで保存が失敗したときメッセージがセットされること(){
 		$this->controller->Post->expects($this->once())->method('save')->will($this->returnValue(false));
--- app/Test/Fixture/PostFixture.php
+++ app/Test/Fixture/PostFixture.php
@@ -12,18 +12,4 @@ class PostFixture extends CakeTestFixture {
  */
 	public $import = array('model' => 'Post');
 
-/**
- * Records
- *
- * @var array
- */
-	public $records = array(
-		array(
-			'id' => 1,
-			'title' => 'Lorem ipsum dolor sit amet',
-			'body' => 'Lorem ipsum dolor sit amet',
-			'modified' => '2015-08-29 15:13:58'
-		),
-	);
-
 }
