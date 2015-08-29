e0c0937fc862d0dcf60e128dc5429edd7be6c5c6
toro23 <23@coidex.com>
2015-08-29 15:19:44 +0000

    postsテーブルのFixtureを更新

--- app/Test/Fixture/PostFixture.php
+++ app/Test/Fixture/PostFixture.php
@@ -6,20 +6,11 @@
 class PostFixture extends CakeTestFixture {
 
 /**
- * Fields
+ * Import
  *
  * @var array
  */
-	public $fields = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
-		'title' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'body' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
-		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1)
-		),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
-	);
+	public $import = array('model' => 'Post');
 
 /**
  * Records
@@ -31,7 +22,7 @@ class PostFixture extends CakeTestFixture {
 			'id' => 1,
 			'title' => 'Lorem ipsum dolor sit amet',
 			'body' => 'Lorem ipsum dolor sit amet',
-			'modified' => '2015-08-29 13:03:06'
+			'modified' => '2015-08-29 15:13:58'
 		),
 	);
 
