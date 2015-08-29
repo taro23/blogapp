28ff9e63aafd0b7aacb1022c0fac9eee27f2f8bb
toro23 <23@coidex.com>
2015-08-29 13:41:47 +0000

    Postモデル追加

---
+++ app/Model/Post.php
@@ -0,0 +1,30 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Post Model
+ *
+ */
+class Post extends AppModel {
+	public $displayField = 'title';
+
+/**
+ * Validation rules
+ */
+	public $validate = [
+		'title' => [
+			'notEmpty' => [
+				'rule' => ['notEmpty'],
+				'message' => 'タイトルは必須入力です',
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			],
+			'maxLength' => [
+				'rule' => ['maxLength', '255'],
+				'message' => 'タイトルは255文字以内で入力してください',
+			],
+		],
+	];
+}
---
+++ app/Test/Fixture/PostFixture.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * PostFixture
+ *
+ */
+class PostFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'title' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'body' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'title' => 'Lorem ipsum dolor sit amet',
+			'body' => 'Lorem ipsum dolor sit amet',
+			'modified' => '2015-08-29 13:03:06'
+		),
+	);
+
+}
