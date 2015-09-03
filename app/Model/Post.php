585c8142745c78e1a00b6bf416f2244eaee268e1
toro23 <23@coidex.com>
2015-09-03 15:10:43 +0000

    DocBlockコメントを追加

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -2,13 +2,27 @@
 App::uses('AppModel', 'Model');
 /**
  * Post Model
+ * 
+ * ブログ記事用モデルです
+ * @copyright php_ci_book
+ * @link https://github.com/phpcibook/blogapp/blob/master/app/Model/Post.php
+ * @since 1.0
+ * @auther 山田太郎 <taro@example.com>
  *
  */
 class Post extends AppModel {
+
+/**
+ * 一覧表示時のタイトルに使用するカラム名
+ * 
+ * @var string 
+ */
 	public $displayField = 'title';
 
 /**
- * Validation rules
+ * バリデーションルール
+ *
+ * @var array
  */
 	public $validate = [
 		'title' => [
